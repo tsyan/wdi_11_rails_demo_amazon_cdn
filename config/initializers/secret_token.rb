4e9c1c835a998ad01d3fd37b6db363977369538a
David Fisher <tibbon@gmail.com>
2013-12-11 09:40:05 -0500

    Created basic scaffolded site

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CDNDemo::Application.config.secret_key_base = 'f8ee3699d910d26f531bc093091521d7912eea9cef40d5e0b2a48df6c1c861cbf1add5eecd8e5d05c884414bc75e92bea81fb7ac420ae12bbb83ee97f9a08afe'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CDNDemo::Application.config.session_store :cookie_store, key: '_CDNDemo_session'
